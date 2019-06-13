97b911a4e3d55af4a3c6ccafea9b467d6a9bea43
Gilberto Javier Morales Hernandez <gilberto morales@99.200-57-1.bestelclientes.com.mx>
2019-06-12 19:32:09 -0500

    send updates

--- 12/server.js
+++ 12/server.js
@@ -2,9 +2,11 @@ const http = require('http');
 
 const server = http.createServer(function (req, res) {
 	const myURL = require('url').parse(req.url);
-	console.log(myURL);	
+	console.log(myURL);
+
 	res.writeHead(200, {'Content-Type':'text/plain'});
 	res.write(myURL.pathname);
+	// res.write('okay')
 	res.end();
 });
 
