2881c720c535ea2ff0bed3632e488e652111e8e6
Gilberto Javier Morales Hernandez <gilberto morales@99.200-57-1.bestelclientes.com.mx>
2019-06-13 19:28:49 -0500

    comiteao netflix

--- 19/server.js
+++ 19/server.js
@@ -1,8 +1,5 @@
 const express = require('express')
 const app = express()
 
-function rootHandler (request, response){
-    response.send('hello world')
-}
 app.get('/',rootHandler)
 app.listen(9000)
